83e97767738eaa96a46f10b814930ba8f6d5a813
Nami.WANG <namiheike@gmail.com>
2018-01-11 17:44:06 +0800

    minor fix

--- lib/views/shared/image_with_loader.dart
+++ lib/views/shared/image_with_loader.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ImageWithLoader extends StatelessWidget {
   final String imgSrc;
 
-  ImageWithLoader(@required this.imgSrc, { Key key }) : super(key: key);
+  ImageWithLoader(this.imgSrc, { Key key }) : super(key: key);
 
   Widget build(BuildContext context) {
     return new FadeInImage(
